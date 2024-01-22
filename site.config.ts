fce4720f4adf6dbdff75ce73bf7c01ec7c2bee8c
mun <mun.xml@gmail.com>
2024-01-22 22:30:47 +0900

    rootNotionPageId 업데이트

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'ac91ff16ddc3468da31b5f936a0f48e7',
+  rootNotionPageId: 'be7b5530c1c443f99311073ec17cb8a0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
